7c50a1d3cddc16e720718f534e65dd3f24c4d3c3
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2019-10-04 12:54:53 +0530

    added help index view

---
+++ src/views/help/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pawara
+ * Date: 10/4/2019
+ * Time: 12:47 PM
+ */
+
+?>
+
+<body class="text-center">
+echo '<div style="padding: 300px; ">';
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+
+        <main role="main" class="inner cover">
+            <h1 class="cover-heading">My PHrame</h1>
+            <p class="lead">        My PHrame is an freely available open source web development frame work with a low amount of code base.
+                This can directly use with acdemic projects to learn the middlewares of Model-View-Controller architecture.
+                Business applications can develop on top of this, after adding your own additional patches.
+                You can contact regarding any help to use or develop with My-PHrame.
+            </p>
+
+            <br><p> For any Concerns <br>Contact us My PHrame: myphrame@gmail.com </p>
+
+        </main>
+
+
+    </div>
