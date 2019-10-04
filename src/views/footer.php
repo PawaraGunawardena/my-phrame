0f7d04f5fdca1c7be8db68a5e6e85df7480bd0ac
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2019-10-04 11:32:03 +0530

    added the footer view for views in the site

---
+++ src/views/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pawara
+ * Date: 10/4/2019
+ * Time: 11:23 AM
+ */
+?>
+
+<html>
+<body>
+
+<footer class="navbar fixed-bottom navbar-expand-sm navbar-dark bg-dark">
+    <div class="container">
+                <span class="text-muted">
+                    | My PHrame | My PHramework | My Framework <br>
+                    | Develop your web application with us |</span>
+    </div>
+</footer>
+
+</body>
+</html>
