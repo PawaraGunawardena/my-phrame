1d6fa8393170fd6b3d4cae041aca82969b1d510d
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2019-10-04 12:59:47 +0530

    added user home page controller

---
+++ src/controllers/home.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pawara
+ * Date: 10/4/2019
+ * Time: 12:46 PM
+ */
+
+
+class Home extends Controller{
+    function __construct(){
+        parent::__construct();
+        Session::init();
+        $logged = Session::get('loggedIn');
+
+        if($logged == false){
+            Session::destroy();
+            header('location: login');
+            exit;
+        }
+    }
+
+    function index(){
+        $this->view->render('home/index');
+    }
+
+    function logout(){
+        Session::destroy();
+        header('location: ../login');
+        exit;
+    }
+}
