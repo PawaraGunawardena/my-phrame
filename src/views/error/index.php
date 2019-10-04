37360aee2dd6b78c4519a46399444a1b3a0f9ded
PawaraGunawardena <pawara.15@cse.mrt.ac.lk>
2019-10-04 12:42:59 +0530

    added view for display the error

---
+++ src/views/error/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pawara
+ * Date: 10/4/2019
+ * Time: 12:23 PM
+ */
+
+?>
+
+<hr/>
+<?php echo $this->msg; ?>
+<hr/>
+
+<div class="alert alert-danger">
+    <strong>
+        Error:
+    </strong>
+    Requested URL not found.
+</div>
